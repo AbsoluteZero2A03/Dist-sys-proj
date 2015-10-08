b59110b88e886ee29ea716a53a9c5167398a8a90
Em Sal <absolutezero2a03@gmail.com>
2015-10-08 11:29:04 -0700

    now running the server tasks in a thread

--- src/broker/broker.cpp
+++ src/broker/broker.cpp
@@ -3,11 +3,14 @@
 #include "json/json.hpp"
 #include "connections.hpp"
 #include "worker.hpp"
+#include <thread>
+#include <functional>
 
 int main () {
     
     std::map<std::string, zmq::socket_t *> curr_connections;
     SkidooshBrokerTask btask;
-    btask.run();
+    std::thread th1(std::bind(&SkidooshBrokerTask::run, &btask));
+    
     return 0;
 }
